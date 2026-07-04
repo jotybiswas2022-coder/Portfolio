08f5c2caa3540a24c8dc0ea1b21ede9a6b6ed0eb
jotybiswas2022-coder <jotybiswas2022@gmail.com>
2026-07-04 15:10:13 +0600

    Add return type to FaqController::index()

--- app/Http/Controllers/admin/FaqController.php
+++ app/Http/Controllers/admin/FaqController.php
@@ -8,7 +8,7 @@ use App\Models\Faq;
 
 class FaqController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): \Illuminate\View\View|\Illuminate\Http\JsonResponse
     {
         $query = $request->input('q');
 
