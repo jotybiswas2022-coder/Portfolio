49fa4d898c5553c8702eead6ed00a7e08a790417
jotybiswas2022-coder <jotybiswas2022@gmail.com>
2026-07-04 15:10:17 +0600

    Add return type to ServiceController::index()

--- app/Http/Controllers/admin/ServiceController.php
+++ app/Http/Controllers/admin/ServiceController.php
@@ -8,7 +8,7 @@ use App\Models\Service;
 
 class ServiceController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): \Illuminate\View\View|\Illuminate\Http\JsonResponse
     {
         $query = $request->input('q');
 
