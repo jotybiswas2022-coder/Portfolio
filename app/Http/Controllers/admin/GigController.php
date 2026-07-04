178b4b34ec73d80f7474cc2edabfbb0e08e6b118
jotybiswas2022-coder <jotybiswas2022@gmail.com>
2026-07-04 15:10:15 +0600

    Add RedirectResponse return type to GigController::store()

--- app/Http/Controllers/admin/GigController.php
+++ app/Http/Controllers/admin/GigController.php
@@ -37,7 +37,7 @@ class GigController extends Controller
         return view('backend.gig.create');
     }
 
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\RedirectResponse
     {
         $request->validate([
             'title'             => 'required|string|max:255',
