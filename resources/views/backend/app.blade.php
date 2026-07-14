36c7b70be3994a80f6882f129dfcdbd166cc679c
jotybiswas2022-coder <jotybiswas2022@gmail.com>
2026-07-14 18:47:40 +0600

    fix: simplify favicon SVG to solid color, use absolute path

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>@yield('title', config('app.name'))</title>
-    <link rel="icon" href="{{ url('/core/favicon.svg') }}" type="image/svg+xml">
+    <link rel="icon" href="/core/favicon.svg" type="image/svg+xml">
+    <link rel="shortcut icon" href="/core/favicon.svg" type="image/svg+xml">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>{{ config('app.name', 'Portfolio') }} | {{ __('messages.home') }}</title>
-    <link rel="icon" href="{{ url('/core/favicon.svg') }}" type="image/svg+xml">
+    <link rel="icon" href="/core/favicon.svg" type="image/svg+xml">
+    <link rel="shortcut icon" href="/core/favicon.svg" type="image/svg+xml">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
